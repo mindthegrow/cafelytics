79abd4b05b923fe7d8f9045ea5b769bb3dee1eaf
David J DeBonis <davidjdebonis@gmail.com>
2020-08-16 13:57:52 -0600

    expand no-data value error; closes #37

--- src/cafe/simulateCoOp.py
+++ src/cafe/simulateCoOp.py
@@ -61,7 +61,19 @@ def main(args):
     output = args.output
     
     if not os.path.exists(farmData):
-        raise ValueError("File: %s does not exist"%farm)
+        raise ValueError(
+        """
+        File: %s does not exist
+        
+        If you are running default commands and this is your first time
+        running the simulation, assure you have run:
+        
+        `python3 src/cafe/fakeData.py --farms 100 --year 2020 --output data/fakeData.csv`
+        
+        in the core directory before calling simulateCoOp.py from the command line.
+        
+        """%farmData)
+        
     
     print("Importing Data")
     farmList = importData.compileCoOp(farmData)
