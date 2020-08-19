46fae72f1f82698431cc909b0e93087a1e0bac18
David J DeBonis <davidjdebonis@gmail.com>
2020-08-19 08:15:11 -0600

    testing validity of test dictionary

--- tests/test_farm.py
+++ tests/test_farm.py
@@ -2,6 +2,7 @@
 
 import pytest
 import cafe.farm as cf
+import cafe.importData as importData
 import numpy as np
 
 __author__ = "Mathematical Michael"
@@ -15,6 +16,11 @@ def test_farm_instantiation_defaults():
     assert test_farm.totalCuerdas == 1
     assert test_farm.pruneYear == None
     assert test_farm.treeType == 'borbon'
+    
+    testDict = importData.openYaml("../../data/trees.yml")
+    test_farm02 = cf.Farm(treeAttributes = testDict)
+    assert test_farm02.treeType == 'borbon'
+    
 
     # this is another syntax structure to accomplish the above
     # but it is new to me, personally -- mm
