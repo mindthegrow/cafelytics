67dd9fc86ecba1984da4ad823d4633accd109b15
David J DeBonis <davidjdebonis@gmail.com>
2020-08-19 19:11:23 -0600

    testing for working directory????

--- tests/test_farm.py
+++ tests/test_farm.py
@@ -13,6 +13,9 @@ __license__ = "mit"
 def test_farm_instantiation_defaults():
     test_farm = cf.Farm()
     # TODO: add some basics here and assert them
+    print("Current working directory:")
+    !pwd
+    
     assert test_farm.totalCuerdas == 1
     assert test_farm.pruneYear == None
     assert test_farm.treeType == 'borbon'
